8e5d8d4aa750f131ef1e8357c04525e0c1a24335
Bao Nhi <yehxiaoer@gmail.com>
2023-11-06 02:28:55 +0700

    Handle connect BLE

--- app/src/main/java/com/example/medicinebox/MedicineBoxFragment.java
+++ app/src/main/java/com/example/medicinebox/MedicineBoxFragment.java
@@ -3,6 +3,9 @@ package com.example.medicinebox;
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
@@ -17,29 +20,50 @@ import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link MedicineBoxFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class MedicineBoxFragment extends Fragment {
+
     private static final String TAG = MedicineBoxFragment.class.getSimpleName();
+    private static final String UUID_DEFAULT = "";
     private static final int REQUEST_BLUETOOTH_SCAN_PERMISSION = 1;
+    private final int statusScan = 1;
+    BluetoothGatt bluetoothGatt;
+    BluetoothLeScanner bluetoothLeScanner;
+    BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                // Device is connected, you can now discover services.
+                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    Log.e(TAG, "onConnectionStateChange: don't have permission to connect");
+                    return;
+                }
+                gatt.discoverServices();
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                // Handle disconnection
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                // Services have been discovered. You can now interact with the device's services and characteristics.
+            }
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
-        BluetoothLeScanner bluetoothLeScanner = BluetoothAdapter.getDefaultAdapter().getBluetoothLeScanner();
-        ScanCallback scanCallback = new ScanCallback() {
-            @Override
-            public void onScanResult(int callbackType, ScanResult result) {
-                // Handle scan results here.
-                BluetoothDevice device = result.getDevice();
-                Log.d(TAG, "onScanResult: " + device);
-                // You can get device information and connect to it here.
-            }
-        };
+        bluetoothLeScanner = BluetoothAdapter.getDefaultAdapter().getBluetoothLeScanner();
+
         if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
             Log.e(TAG, "onCreate: Don't have permission to scan");
             requestPermissions(new String[]{Manifest.permission.BLUETOOTH_SCAN}, REQUEST_BLUETOOTH_SCAN_PERMISSION);
@@ -65,5 +89,36 @@ public class MedicineBoxFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_medicine_box, container, false);
-    }
+    }    ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            // Handle scan results here.
+            BluetoothDevice device = result.getDevice();
+            Log.d(TAG, "onScanResult: " + device);
+            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                Log.e(TAG, "onScanResult: don't have permission to connect");
+                return;
+            }
+            if (device.getUuids().equals(UUID_DEFAULT)) {
+                bluetoothGatt = device.connectGatt(requireContext(), false, gattCallback);
+                bluetoothLeScanner.stopScan(scanCallback);
+            }
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+            // Handle batch scan results
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+            // Handle scan failure
+        }
+    };
+
+
+
+
 }
